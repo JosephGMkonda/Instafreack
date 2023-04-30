9415ca79ccadc381e04712550a9f952acadcb172
sangwaniKondowe <josephgmkonda@gmail.com>
2023-04-30 10:13:34 +0400

    .

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,8 +3,38 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Registration from "./pages/Registration";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+
 function App() {
-  return <Home/>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+
+        </Route>
+        <Route path='/login'>
+          <Login/>
+
+        </Route>
+        <Route path='/register'>
+          <Registration/>
+
+        </Route>
+
+        <Route path='/profile'>
+          <Profile/>
+
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
